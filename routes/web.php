600878d4edd7f10b3787c1564ee5c461d3b787ec
noran66 <mohamednoran476@gmail.com>
2025-09-30 01:13:55 +0300

    design edit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+// Route::get('/shop', function () {
+//     return view('shop');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
